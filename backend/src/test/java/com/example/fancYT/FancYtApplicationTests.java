580f65be5781b3ea0868eab24ee20af979bca080
Julie <juliegit@fastmail.com>
2024-03-19 16:07:10 -0400

    Début du framework

---
+++ backend/src/test/java/com/example/fancYT/FancYtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.fancYT;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FancYtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
